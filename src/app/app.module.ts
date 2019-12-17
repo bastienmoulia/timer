b1230a2c4e2e158aef5ff12fe73ef46f203d60d4
Bastien <bmoulia@gmail.com>
2019-12-17 16:24:58 +0100

    service worker relative path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     FormsModule,
     CoreModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
